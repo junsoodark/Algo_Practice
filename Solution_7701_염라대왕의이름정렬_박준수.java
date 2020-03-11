1681675c66311b251fc38dcdc5a9e5342dda8973
junsoodark <61306494+junsoodark@users.noreply.github.com>
2020-03-11 17:34:09 +0900

    Update Solution_7701_염라대왕의이름정렬_박준수.java

--- Solution_7701_염라대왕의이름정렬_박준수.java
+++ Solution_7701_염라대왕의이름정렬_박준수.java
@@ -2,15 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Solution_7701_염라대왕의이름정렬_박준수 {
-	static HashSet<String> hs;
 	static PriorityQueue<str> pq = new PriorityQueue<str>();
-	static List<String> arrlist;
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -19,22 +14,21 @@ public class Solution_7701_염라대왕의이름정렬_박준수 {
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			sb.append('#').append(t).append('\n');
-			hs = new HashSet<String>();
-			arrlist = new ArrayList<String>();
 			int n = Integer.parseInt(br.readLine());
 			for (int i = 0; i < n; i++) {
-				hs.add(br.readLine());
+				pq.offer(new str(br.readLine()));
 			}
-			arrlist.addAll(hs);
-			for (int i = 0; i < hs.size(); i++) {
-				pq.offer(new str(arrlist.get(i)));
-			}
+			String s = "";
 			while (!pq.isEmpty()) {
-				sb.append(pq.poll()).append('\n');
+				String tmps = pq.poll().toString();
+				if (tmps.equals(s)) {
+					continue;
+				}
+				s = tmps;
+				sb.append(tmps).append('\n');
 			}
 		}
 		System.out.println(sb);
-
 	}
 
 	public static class str implements Comparable<str> {
@@ -65,11 +59,6 @@ public class Solution_7701_염라대왕의이름정렬_박준수 {
 				return 1;
 			}
 			return 0;
-		}
-
-		@Override
-		public String toString() {
-			return str;
 		}
 	}
 }
