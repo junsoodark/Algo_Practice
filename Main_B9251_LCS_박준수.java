e3c0e5d429b39dba6dee51e42234beb987731ba3
junsoodark <61306494+junsoodark@users.noreply.github.com>
2020-08-13 03:33:55 +0900

    Update Main_B9251_LCS_박준수.java

--- Main_B9251_LCS_박준수.java
+++ Main_B9251_LCS_박준수.java
@@ -1,32 +1,29 @@
 package algo;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
 public class Main_B9251_LCS_박준수 {
 	static BufferedReader br;
-	static char[] aArr;
-	static char[] bArr;
-	static int dpArr[][];
+	static char[] arr1, arr2;
+	static int[][] arr;
 
 	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input/input_B_9251.txt"));
 		br = new BufferedReader(new InputStreamReader(System.in));
-		aArr = br.readLine().toCharArray();
-		bArr = br.readLine().toCharArray();
-		dp();
-		System.out.println(dpArr[aArr.length][bArr.length]);
-	}
-
-	public static void dp() {
-
-		for (int i = 1; i <= aArr.length; i++) {
-			for (int j = 1; j <= bArr.length; j++) {
-				if (aArr[i - 1] == bArr[j - 1]) {
-					dpArr[i][j] = dpArr[i - 1][j - 1] + 1;
+		arr1 = br.readLine().toCharArray();
+		arr2 = br.readLine().toCharArray();
+		arr = new int[arr2.length + 1][arr1.length + 1];
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = 0; j < arr1.length; j++) {
+				if (arr1[j] == arr2[i]) {
+					arr[i + 1][j + 1] = arr[i][j] + 1;
 				} else {
-					dpArr[i][j] = Math.max(dpArr[i - 1][j], dpArr[i][j - 1]);
+					arr[i + 1][j + 1] = Math.max(arr[i + 1][j], arr[i][j + 1]);
 				}
 			}
 		}
+		System.out.println(arr[arr2.length][arr1.length]);
 	}
 }
